13b430157e13ed1202d5d8e55e633777b649c5a3
Keyvan Fatehi <keyvanfatehi@gmail.com>
2024-05-23 14:22:11 +0000

    fixes websock instability that would crash rtc core unnecessarily

--- python/websocket_signaling.py
+++ python/websocket_signaling.py
@@ -2,14 +2,25 @@ import asyncio
 import websockets
 
 from aiortc.contrib.signaling import object_from_string, object_to_string
+from asyncio.exceptions import IncompleteReadError
+from websockets.exceptions import ConnectionClosedError
 
 class WebsocketSignaling:
     def __init__(self, server):
         self._server = server
         self._websocket = None
+        self._reconnect_attempts = 0
 
     async def connect(self):
-        self._websocket = await websockets.connect(self._server)
+        while self._reconnect_attempts < 5:
+            try:
+                self._websocket = await websockets.connect(self._server)
+                self._reconnect_attempts = 0 # reset attempts on success
+                return
+            except (ConnectionClosedError, OSError) as e:
+                self._reconnect_attempts += 1
+                wait_time = min(2 ** self._reconnect_attempts, 30)
+                await asyncio.sleep(wait_time)
 
     async def close(self):
         if self._websocket is not None and self._websocket.open is True:
@@ -19,14 +30,14 @@ class WebsocketSignaling:
     async def receive(self):
         try:
             data = await self._websocket.recv()
-        except asyncio.IncompleteReadError:
-            return
-        ret = object_from_string(data)
-        if ret == None:
-            print("remote host says good bye!")
-
-        return ret
+            return object_from_string(data)
+        except (ConnectionClosedError, IncompleteReadError) as e:
+            self.connect()
+            return None
 
     async def send(self, descr):
         data = object_to_string(descr)
-        await self._websocket.send(data + '\n')
+        try:
+            await self._websocket.send(data + '\n')
+        except ConnectionClosedError:
+            await self.connect()
